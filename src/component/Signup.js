d1458066c8885bf0789487899d85e857ec672e03
Zameer Ahmad <118186761+Zamahmad12@users.noreply.github.com>
2025-09-01 16:30:44 +0530

    Update Signup.js

--- src/component/Signup.js
+++ src/component/Signup.js
@@ -25,7 +25,7 @@ const Signup = () => {
 
       if (profilePic) {
         // 1️⃣ Get Cloudinary signature from backend
-        const sigRes = await fetch(
+        const sigRes = await
           fetch(`${process.env.REACT_APP_API_URL}/api/cloudinary/signature`)
         );
         const sigData = await sigRes.json();
